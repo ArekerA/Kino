bd96958c2deeb4c22e699f468f5c5e0393e4568e
ArekerA <Arek.ludwikowski@hotmail.com>
2018-04-14 13:34:06 +0200

    zmiana menu

---
+++ menu.js
@@ -0,0 +1,6 @@
+document.getElementById("menu").innerHTML = '<li><a href="index.html">Aktualności</a></li>'+
+    '<li><a href="filmy.html">Filmy</a></li>'+
+    '<li><a href="repertuar.html">Repertuar</a></li>'+
+    '<li><a href="cennik.html">Cennik</a></li>'+
+    '<li><a href="infro.html">Informacje</a></li>'+
+    '<li><a href="zaloguj.html">Zaloguj</a></li>';
