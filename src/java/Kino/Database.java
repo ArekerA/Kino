47285312d80e7f95e63f7b079730b98a0ae959c9
ArekerA <Arek.ludwikowski@hotmail.com>
2018-05-20 15:09:02 +0200

    edit Database.java

--- src/java/Kino/Database.java
+++ src/java/Kino/Database.java
@@ -77,9 +77,9 @@ public class Database {
                 "<p>Kino Planeta</p>\n" +
                 "<p>Kraków, Warszawska 24</p>\n" +
                 "<a href=\"mailto:someone@example.com?Subject=Hello%20again\">Napisz do nas</a>\n" +
-                "</div></aside><section><article>\n" +
+                "</div></aside><div>\n" +
                 "<iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d640.208376698259!2d19.942613645244574!3d50.07067962269475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47165b04a1f6a23f%3A0xea28a4cede108adf!2sWydzia%C5%82+In%C5%BCynierii+Elektrycznej+i+Komputerowej!5e0!3m2!1sen!2spl!4v1523705858639\" width=\"100%\" height=\"450\" frameborder=\"0\" style=\"border:0\" allowfullscreen></iframe>\n" +
-                "</article></section>');");
+                "</div>');");
             st.executeUpdate("INSERT INTO Bilety VALUES ( 0, 'normalny', 12.50);");
             st.executeUpdate("INSERT INTO Userzy VALUES ( 0, 'admin', 'admin@example.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 3),( 2, 'prac', 'admin@example.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 2);");
             st.executeUpdate("INSERT INTO Zamowienia VALUES ( 0, 0);");
